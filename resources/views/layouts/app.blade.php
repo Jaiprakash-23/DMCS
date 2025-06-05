ca193c43a4fed3f51f0f7703e4eff8a501f8aa4b
Jai Prakash <110091591+JaiPrakash23@users.noreply.github.com>
2025-06-05 19:11:27 +0530

    Update DMCS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,11 +31,13 @@
     <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+    @vite(['resources/sass/app.css', 'resources/js/app.js']) --}}
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-    
+
 </head>
 
 <body>
